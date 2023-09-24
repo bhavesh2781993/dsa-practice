af0111a1f31bd61d3b0cb47eff282c76fd23962c
Bhavesh <shah27bhavesh@gmail.com>
2023-09-24 22:57:13 +0530

    insertion sort

---
+++ src/main/java/algorithms/sorting/P003_InsertionSort.java
@@ -0,0 +1,29 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+public class P003_InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 4, 5, 66, 1};
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
