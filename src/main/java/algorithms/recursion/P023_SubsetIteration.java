68448d6c6bd16604c017575f72c442da00a441ff
Bhavesh <shah27bhavesh@gmail.com>
2023-10-10 09:48:20 +0530

    subset iteration

---
+++ src/main/java/algorithms/recursion/P023_SubsetIteration.java
@@ -0,0 +1,48 @@
+package algorithms.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P023_SubsetIteration {
+    public static void main(String[] args) {
+        subset(new int[] {1, 2, 3});
+        subsetDupicate(new int[] {1, 2, 2});
+    }
+
+    private static void subset(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int i = 0; i < arr.length; i++) {
+            int iterations = result.size();
+            for (int j = 0; j < iterations; j++) {
+                List<Integer> tempResult = new ArrayList<>();
+                tempResult.addAll(result.get(j));
+                tempResult.add(arr[i]);
+                result.add(tempResult);
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void subsetDupicate(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = result.size() - 1;
+            int iterations = result.size();
+            for (int j = start; j < iterations; j++) {
+                List<Integer> tempResult = new ArrayList<>();
+                tempResult.addAll(result.get(j));
+                tempResult.add(arr[i]);
+                result.add(tempResult);
+            }
+        }
+        System.out.println(result);
+    }
+}
