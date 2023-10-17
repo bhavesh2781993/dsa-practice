7533b572f43bc77c14c6744577154e924fcc6533
Bhavesh <shah27bhavesh@gmail.com>
2023-10-17 12:23:21 +0530

    recursion: maze with diagonal path

--- src/main/java/leetcode/P010_Maze.java
+++ src/main/java/leetcode/P010_Maze.java
@@ -10,6 +10,9 @@ public class P010_Maze {
 
         final List<String> ways = ways(3, 3, "");
         System.out.println(ways);
+
+        final List<String> diagonalWays = waysDiagonal(3, 3, "");
+        System.out.println(diagonalWays);
     }
 
     private static int countWays(int row, int column) {
@@ -36,6 +39,25 @@ public class P010_Maze {
         } else {
             tempResult.addAll(ways(row - 1, col, result + "D"));
             tempResult.addAll(ways(row, col - 1, result + "R"));
+        }
+
+        return tempResult;
+    }
+
+    private static List<String> waysDiagonal(int row, int col, String result) {
+        if (row == 1 && col == 1) {
+            return List.of(result);
+        }
+
+        List<String> tempResult = new ArrayList<>();
+        if (row == 1) {
+            tempResult.addAll(waysDiagonal(row, col - 1, result + "R"));
+        } else if (col == 1) {
+            tempResult.addAll(waysDiagonal(row - 1, col, result + "D"));
+        } else {
+            tempResult.addAll(waysDiagonal(row - 1, col, result + "D"));
+            tempResult.addAll(waysDiagonal(row - 1, col - 1, result + "d"));
+            tempResult.addAll(waysDiagonal(row, col - 1, result + "R"));
         }
 
         return tempResult;
